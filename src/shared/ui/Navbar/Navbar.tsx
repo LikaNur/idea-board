b26f0fb4eb1b003463ccb5da78e1676e254e5b2c
Lika <123624510+LikaNur@users.noreply.github.com>
2025-07-18 02:31:46 +0100

    fix: link to the homepage

--- src/shared/ui/Navbar/Navbar.tsx
+++ src/shared/ui/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar({
 
   return (
     <nav className="flex relative z-50 items-stretch justify-between bg-white/10 backdrop-blur-sm backdrop-opacity-0 h-[90px] text-white px-3 md:px-48">
-      <a href="/" className="flex items-center pointer">
+      <a href="/idea-board" className="flex items-center pointer">
         <IdeaLogo className="w-6 h-6 md:w-8 md:h-8" />
         <h1 className="text-md font-bold tracking-wider md:text-xl">
           IdeaBoard
